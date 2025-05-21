5c8d1e9044c5f91fb208e62560c842649af42e84
KonetiBalaji <balajishiva2580@gmail.com>
2025-05-21 01:26:40 -0700

    ensure black theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,27 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
  // darkMode: ["selector", '[data-mode="dark"]'],
  darkMode: 'class',
   content: [
-    "./src/app/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {},
+  theme: {
+    extend: {
+      keyframes: {
+        blink: {
+          '0%, 100%': { opacity: 1 },
+          '50%': { opacity: 0 },
+        }
+      },
+      animation: {
+        blink: 'blink 1s step-end infinite',
+      },
+      fontFamily: {
+        sans: ['var(--font-inter)', 'Inter', 'sans-serif'],
+      },
+    },
+  },
   plugins: [],
 };
